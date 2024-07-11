775a78408fe94fdb534a043cced394e26812a423
Eduardo Silva <edsiper@gmail.com>
2024-07-11 17:58:41 -0600

    otlp-to-otlp: update scenarios
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

--- scenarios/otlp-to-otlp/tests/test_logs_001.py
+++ scenarios/otlp-to-otlp/tests/test_logs_001.py
@@ -18,9 +18,9 @@ import os
 import sys
 import json
 import logging
-import requests
 import time
-import threading
+import base64
+import requests
 
 # OTel imports to convert from JSON to OTLP Protobuf
 from opentelemetry.proto.collector.logs.v1.logs_service_pb2 import ExportLogsServiceRequest
@@ -38,15 +38,6 @@ from otlp_server import otlp_server_run, data_storage
 
 logger = logging.getLogger(__name__)
 
-def send_request(port, payload):
-    # Send the protobuf payload
-    url = f'http://localhost:{port}/v1/logs'
-    headers = {'Content-Type': 'application/x-protobuf'}
-    response = requests.post(url, data=payload.SerializeToString(), headers=headers)
-    print(f'Status code: {response.status_code}')
-    print(f'Response text: {response.text}')
-
-
 #  [Fluent Bit Test Suite]
 #    - Start Fluent Bit:
 #      - a custom configuration file
@@ -61,61 +52,115 @@ def send_request(port, payload):
 #
 #    In the process above, Fluent Bit decode the request, process it and encode it back.
 
-def run_unit_test(file_data_in, config_file):
-    # this thing needs to be refactored through a proper API
-    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../tests', 'data_files'))
+class Service:
+    def __init__(self, config_file):
+        # Compose the absolute path for the Fluent Bit configuration file
+        self.config_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '../config/', config_file))
+        data_storage['logs'] = []
 
-    # Load the JSON payload and convert it to protobuf
-    json_payload_dict = read_json_file(os.path.join(base_path, file_data_in))
-    json_payload_str = json.dumps(json_payload_dict)
+    def start(self):
+        # Create an instance of the Fluent Bit Manager
+        self.flb = FluentBitManager(self.config_file)
 
-    log_request = ExportLogsServiceRequest()
-    protobuf_payload = json_format.Parse(json_payload_str, log_request)
+        # set a listener port (input plugin) for the config file (note: we use an environment variable
+        # so Fluent Bit will replace it in the config once it loads it)
+        self.flb_listener_port = find_available_port(starting_port=45000)
+        os.environ['FLUENT_BIT_TEST_LISTENER_PORT'] = str(self.flb_listener_port)
+        logger.info(f"Fluent Bit listener port: {self.flb_listener_port}")
 
-    # Compose the absolute path for the Fluent Bit configuration file
-    config_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '../config/', config_file))
-    #logger.info(f"config file {config_file}")
+        # find and set a listener port for the local server to receive the
+        # Fluent Bit output
+        self.test_suite_http_port = find_available_port(starting_port=50000)
+        os.environ['TEST_SUITE_HTTP_PORT'] = str(self.test_suite_http_port)
+        logger.info(f"test suite http port: {self.test_suite_http_port}")
+
+        # Start an OTLP Server (receiver)
+        otlp_server_run(self.test_suite_http_port)
+
+        # Ping the test suite server
+        url = f'http://127.0.0.1:{self.test_suite_http_port}/ping'
+
+        # try to ping the server
+        while True:
+            try:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    break
+            except requests.exceptions.ConnectionError:
+                time.sleep(0.5)
+                logger.info("waiting for server...")
 
 
-    # Create an instance of the Fluent Bit Manager
-    flb = FluentBitManager(config_file)
+        # Start Fluent Bit
+        self.flb.start()
 
-    # set a listener port (input plugin) for the config file (note: we use an environment variable
-    # so Fluent Bit will replace it in the config once it loads it)
-    flb_listener_port = find_available_port(starting_port=45000)
-    os.environ['FLUENT_BIT_TEST_LISTENER_PORT'] = str(flb_listener_port)
-    logger.info(f"Fluent Bit listener port: {flb_listener_port}")
+    def read_response(self):
+        while len(data_storage['logs']) <= 0:
+            time.sleep(0.5)
+            logger.info("waiting for response...")
 
-    # find and set a listener port for the local server to receive the
-    # Fluent Bit output
-    test_suite_http_port = find_available_port(starting_port=50000)
-    os.environ['TEST_SUITE_HTTP_PORT'] = str(test_suite_http_port)
-    logger.info(f"test suite http port: {test_suite_http_port}")
+        json_str = json_format.MessageToJson(data_storage['logs'][0])
+        logger.info(f"{json_str}")
+        return json.loads(json_str)
 
-    # # Start an OTLP Server (receiver)
-    flask_thread = threading.Thread(target=otlp_server_run, kwargs={"port": int(test_suite_http_port)})
-    flask_thread.daemon = True
-    flask_thread.start()
+    def send_request(self, payload, content_type='application/x-protobuf'):
+        # Send the protobuf payload
+        url = f'http://localhost:{self.flb_listener_port}/v1/logs'
+        headers = {'Content-Type': content_type}
+        response = requests.post(url, data=payload.SerializeToString(), headers=headers)
+        print(f'Status code: {response.status_code}')
+        print(f'Response text: {response.text}')
 
-    # Start Fluent Bit
-    flb.start()
+    def send_json_as_otel_protobuf(self, json_input):
+        base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../tests', 'data_files'))
+        json_payload_dict = read_json_file(os.path.join(base_path, json_input))
+        json_payload_str = json.dumps(json_payload_dict)
 
-    send_request(flb_listener_port, protobuf_payload)
+        log_request = ExportLogsServiceRequest()
+        protobuf_payload = json_format.Parse(json_payload_str, log_request)
 
-    # a dummy wait
-    while len(data_storage['logs']) <= 0:
-        time.sleep(0.5)
-        logger.info("waiting for response...")
+        self.send_request(protobuf_payload)
 
-    json_str = json_format.MessageToJson(data_storage['logs'][0])
+        return self.read_response()
 
-    # stop Fluent Bit
-    flb.stop()
+    def stop(self):
+        self.flb.stop()
+        requests.post(f'http://localhost:{self.test_suite_http_port}/shutdown')
 
-    return json.loads(json_str)
 
-def test_basic_log():
-    output = run_unit_test("test_logs_001.in.json", "fluent-bit.yaml")
-    logger.info(f"test: {output}")
+# This is a full pipeline test, the file tests_logs_001.in.json, represents an OpenTelemetry Log payload in
+# JSON format which gets converted to Protobuf.
+#
+# Then Fluent Bit is started having an OpenTelemetry listener (input plugin) and OpenTelemetry output plugin
+# that sends the data back to the test suite. Note that we instance a dummy OTLP server in this test suite
+# so we can check the data that is being sent back.
+def test_opentelemetry_to_opentelemetry_basic_log():
+    service = Service("001-fluent-bit.yaml")
+    service.start()
+    output = service.send_json_as_otel_protobuf("test_logs_001.in.json")
+    logger.info(f"response: {output}")
+    service.stop()
+
     assert len(output['resourceLogs']) == 2
 
+
+# Start a Fluent Bit Pipeline with Dummy message and then it gets handle by OpenTelemetry output, the config
+# aims to populate traceId and spanId fields with the values from the Dummy message.
+#
+# issue : https://github.com/fluent/fluent-bit/issues/9071
+# fixed : https://github.com/fluent/fluent-bit/pull/9074
+def test_dummy_to_opentelemetry_log():
+    service = Service("002-fluent-bit.yaml")
+    service.start()
+    output = service.read_response()
+    logger.info(f"response: {output}")
+    service.stop()
+
+    # direct reference to the record
+    record = output['resourceLogs'][0]['scopeLogs'][0]['logRecords'][0]
+
+    # notes on traceid and spanid: the test case encodes the values as hex strings, Fluent Bit OpenTelemetry
+    # output plugin will decode and pack them as bytes. When the data is sent back to the test suite, the values
+    # are encoded as base64 strings (Python thing). So we need to decode them back to bytes and compare them.
+    assert base64.b64decode(record['traceId']) == bytes.fromhex('63560bd4d8de74fae7d1e4160f2ee099')
+    assert base64.b64decode(record['spanId'])  == bytes.fromhex('251484295a9df731')
--- src/server/otlp_server.py
+++ src/server/otlp_server.py
@@ -14,8 +14,12 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import time
 import logging
+import threading
+
 from flask import Flask, request, jsonify
+from waitress import serve
 from google.protobuf import json_format
 from opentelemetry.proto.collector.trace.v1.trace_service_pb2 import ExportTraceServiceRequest
 from opentelemetry.proto.collector.metrics.v1.metrics_service_pb2 import ExportMetricsServiceRequest
@@ -24,6 +28,30 @@ from opentelemetry.proto.collector.logs.v1.logs_service_pb2 import ExportLogsSer
 app = Flask(__name__)
 data_storage = {"traces": [], "metrics": [], "logs": []}
 logger = logging.getLogger(__name__)
+
+server_thread = None
+shutdown_flag = threading.Event()
+
+from flask import Flask, request, jsonify
+import threading
+import time
+import requests
+import logging
+from waitress import serve
+
+app = Flask(__name__)
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+shutdown_flag = threading.Event()
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    shutdown_flag.set()
+    logger.info("OTLP Server is shutting down...")
+    return jsonify({"status": "shutting down"}), 200
 
 @app.route('/v1/traces', methods=['POST'])
 def traces():
@@ -47,10 +75,21 @@ def logs():
     log_request = ExportLogsServiceRequest()
     log_request.ParseFromString(data)
     data_storage["logs"].append(log_request)
-
-    #logger.info(f"Received log:\n {json_format.MessageToJson(log_request)}")
     return jsonify({"status": "received"}), 200
 
-def otlp_server_run(port=4317):
-    logger.info("Starting OTLP server")
-    app.run(port=port)
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({"status": "pong"}), 200
+
+def run_server(port=4317):
+    serve(app, host='0.0.0.0', port=port, threads=1)
+
+def otlp_server_run(port):
+    global server_thread
+
+    logger.info(f"Starting OTLP server on port {port}")
+    server_thread = threading.Thread(target=run_server, kwargs={"port": port})
+    server_thread.daemon = True
+    server_thread.start()
+
+    return server_thread
